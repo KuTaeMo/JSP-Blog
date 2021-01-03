16c2fd687a12d8cee788a75482d3f31cccfb892b
TAEMO KU <ktm6044@daum.net>
2021-01-03 18:44:08 +0900

    2. JSP 블로그 프로젝트 구조 잡기 (2/2)

--- src/com/cos/blog/web/ReplyController.java
+++ src/com/cos/blog/web/ReplyController.java
@@ -1,6 +1,7 @@
 package com.cos.blog.web;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
