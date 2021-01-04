8529f4b2bd830909d234d8a0604100f22ab5c76c
kutaemo <ktm6044@daum.net>
2021-01-04 15:10:10 +0900

    5. 주소 API 사용

--- src/com/cos/blog/util/Script.java
+++ src/com/cos/blog/util/Script.java
@@ -6,13 +6,18 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletResponse;
 
 public class Script {
-	public static void back(HttpServletResponse resp,String msg) throws IOException {
-		PrintWriter out=resp.getWriter();
-		out.println("<script>");
-		out.println("alert(`"+msg+"`);");
-		out.println("history.back();");
-		out.println("</script>");
-		out.flush();
-	}
 
+	public static void back(HttpServletResponse response, String msg) {
+		PrintWriter out;
+		try {
+			out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('" + msg + "');");
+			out.println("history.back();");
+			out.println("</script>");
+			out.flush(); // 버퍼 비우기
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
