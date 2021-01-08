eb94f67e729bbdfe5a55d1a360bca27cc908aa0f
KuTaeMo <67215505+KuTaeMo@users.noreply.github.com>
2021-01-08 11:56:09 +0900

    12. 글 상세보기 완료

--- src/com/cos/blog/service/BoardService.java
+++ src/com/cos/blog/service/BoardService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.cos.blog.domain.board.Board;
 import com.cos.blog.domain.board.BoardDao;
+import com.cos.blog.domain.board.dto.DetailRespDto;
 import com.cos.blog.domain.board.dto.SaveReqDto;
 
 public class BoardService {
@@ -22,5 +23,9 @@ public class BoardService {
 	}
 	public int 글개수() {
 		return boardDao.count();
+	}
+	public DetailRespDto 글상세보기(int id) {
+		// 조회수 업데이트치기
+		return boardDao.findById(id);
 	}
 }
